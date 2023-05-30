2bc91b4f5a7069aaf70f6cb80e95de99a95bdb17
paola <pebitaa5795@hotmail.com>
2023-05-29 21:45:59 -0600

    se creo el doc para buscar

---
+++ controladores/clientes/buscar.php
@@ -0,0 +1,68 @@
+<?php
+require '../../modelos/clientes.php';
+try {
+    $clientes = new clientes($_GET);
+    
+    $clientess = $clientes->buscar();
+    // echo "<pre>";
+    // var_dump($clientess);
+    // echo "</pre>";
+    // exit;
+    // $error = "NO se guardó correctamente";
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2){
+    $error = $e2->getMessage();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Resultados</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <table class="table table-bordered table-hover">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>NO. </th>
+                            <th>NOMBRE</th>
+                            <th>NIT</th>
+                            <th>SITUACION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if(count($clientes) > 0):?>
+                        <?php foreach($clientes as $key => $clientes) : ?>
+                        <tr>
+                            <td><?= $key + 1 ?></td>
+                            <td><?= $clientes['clientes_NOMBRE'] ?></td>
+                            <td><?= $clientes['clientes_PRECIO'] ?></td>
+                            <td><a class="btn btn-warning w-100" href="/CRUD_CLIENTES/vistas/clientes/modificar.php?clientes_id=<?= $clientes['clientes_ID']?>">Modificar</a></td>
+                            <td><a class="btn btn-danger w-100" href="/CRUD_CLIENTES/controladores/clientes/eliminar.php?clientes_id=<?= $clientes['clientes_ID']?>">Eliminar</a></td>
+                        </tr>
+                        <?php endforeach ?>
+                        <?php else :?>
+                            <tr>
+                                <td colspan="3">NO EXISTEN REGISTROS</td>
+                            </tr>
+                        <?php endif?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+                <a href="/CRUD_CLIENTES/vistas/clientes/buscar.php" class="btn btn-info w-100">Volver al formulario</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
