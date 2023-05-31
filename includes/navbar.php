324ca245f88e71842caafce3bc255902eb25844a
paola <pebitaa5795@hotmail.com>
2023-05-31 08:59:52 -0600

    se configuro si el nit es valido

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,13 +4,13 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Tienda virtual</a>
+            <a class="navbar-brand" href="#">APLICACION CLIENTES</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#"></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/CRUD_CLIENTES/Vistas/clientes/index.php">Ventas</a>
+                    <a class="nav-link" aria-current="page" href="/CRUD_CLIENTES/Vistas/clientes/index.php"></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -18,7 +18,7 @@
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/CRUD_CLIENTES/Vistas/clientes/index.php">CLIENTES</a>
+                            <a class="nav-link" href="/CRUD_CLIENTES/Vistas/clientes/index.php">Ingreso de Clientes</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/CRUD_CLIENTES/Vistas/clientes/buscar.php">Buscar Clientes</a>
